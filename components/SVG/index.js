7ec72b858bcf7bc8b184f14cbecad2a899a3581c
Daulat Hussain <102717032+daulathussain@users.noreply.github.com>
2025-08-27 16:56:17 +0530

    Update index.js

--- components/SVG/index.js
+++ components/SVG/index.js
@@ -3,5 +3,7 @@ import Two from "./Two";
 import Three from "./Three";
 import Four from "./Four";
 import Five from "./Five";
+import Six from "./Six";
+import Seven from "./Seven";
 
-export { One, Two, Three, Four, Five };
+export { One, Two, Three, Four, Five, Six, Seven };
